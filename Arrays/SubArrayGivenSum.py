8a9d20b3a31233e13be0a3e87c95b8df05218755
timmcallister <tim.mcallister84@gmail.com>
2019-05-23 02:33:57 -0500

    Finished Sub Array of Given Sum

---
+++ Arrays/SubArrayGivenSum.py
@@ -0,0 +1,28 @@
+tests_input = int(input())
+
+size_input, sum_input, array_input = ([None] * tests_input for i in range(3))
+result = [-1] * tests_input
+
+for i in range(tests_input):
+    size_input[i], sum_input[i] = list(map(int, input().split()))
+    array_input[i] = list(map(int, input().split()))
+  
+found = False
+
+for ind in range(tests_input):
+    size = len(array_input[ind])
+    for i in range(size):
+        for j in range(size):
+            if i+j >= size:
+                break
+            s = sum(array_input[ind][i:i+j+1])
+            if s == sum_input[ind]:
+                found = True
+                result[ind] = str(i+1) + " " + str(i+j+1)
+                break
+        if found:
+            break
+    print(result[ind])
+    
+    
+
